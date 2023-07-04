449a0519af458a0800623804679c76cd559e8796
B Tasker <88340935+btasker@users.noreply.github.com>
2023-07-04 17:58:48 +0100

    Start buildging

---
+++ html/static/main.js
@@ -0,0 +1,46 @@
+// Main
+
+function fetchData(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            console.log(data);
+            updateResponseTimePlot(data['edge_response_times']);
+        }
+    };
+
+    data = xmlhttp.open("GET", "output.json", true);
+    xmlhttp.send()
+}
+
+
+function updateResponseTimePlot(data){
+    regions = {}
+
+    data.forEach(function(val){
+        // Instantiate the object if needed
+        if (!(val["region"] in regions)){
+            regions[val["region"]] = {
+                 x: [],
+                 y: [],
+                 name: val["region"],
+                 mode: "lines"
+            }
+        }
+
+        // Push data in
+        regions[val["region"]].x.push(val['time'])
+        regions[val["region"]].y.push(val['response_time'])
+
+    });
+
+    odata = []
+    for ([region, val] of Object.entries(regions)){
+        odata.push(regions[val]);
+    }
+
+    Plotly.newPlot('response_times', odata)
+}
+
+fetchData()
