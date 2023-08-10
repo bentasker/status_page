eafb7faac9ebbff3f2b87e92947a3c64f7022222
B Tasker <github@bentasker.co.uk>
2023-08-10 14:44:36 +0100

    The probe is occasionally writing a string in for some reason. Caste to float to fix graphs
    Probe will be fixed seperately

--- app/status_page.py
+++ app/status_page.py
@@ -29,6 +29,7 @@ def get_probe_status(query_api, url):
         |> filter(fn: (r) => r._measurement == "http_reachability_check")
         |> filter(fn: (r) => r["_field"] == "probe_status")
         |> filter(fn: (r) => r["url"] == "{url}")
+        |> map(fn: (r) => ({r with _value: float(v: r._value)}))
         |> group()
         |> mean()    
     '''
@@ -62,6 +63,7 @@ def get_service_status(query_api, url):
         |> filter(fn: (r) => r["_field"] == "response_status_code_match")
         |> filter(fn: (r) => r["server"] == "{url}")
         |> last()
+        |> map(fn: (r) => ({r with _value: float(v: r._value)}))
         |> group()
         |> mean()
     '''
